7daf4ff0be13616fdd96de130d4c36eec24c32a2
Daphne <daphne88veronica@gmail.co>
2021-03-13 20:35:10 -0600

    first commit

---
+++ FitDeck_CSCI4805/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FitDeck_CSCI4805
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
